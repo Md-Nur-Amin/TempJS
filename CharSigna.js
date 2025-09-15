b0fc498c2317c5113b54043508a6dc69c4759577
Nur-Amin2 <rabbiii049@gmail.com>
2025-09-16 01:17:16 +0600

    comments added and few changes has done

--- CharSigna.js
+++ CharSigna.js
@@ -1,20 +1,20 @@
-const transmission = "pQwErT123ZxY";
+const transmission = "pQwErT123ZxY"; // given value
+console.log(transmission.length); // check
 
-function analyzeFluxEvents(transmission) {
-    let n = 1;
-    for (let i = 0; i < transmission.length; i++) {
-        const ascii = transmission[i];
-        if(ascii>="A" && ascii<="Z"){
-            n++
+function analyzeFluxEvents(transmission) { // given function name
+    let n = 0; 
+    for (let i = 0; i < transmission.length; i++) { // loop through each character
+        const ascii = transmission[i]; //
+        if (ascii >= "A" && ascii <= "Z") { // conditions check with uppercase and lowercase, if I use lowercase the output shows more than 5, so I assume it's uppercase only
+            n++ ; // increment n value
         }
     }
     if(n>0){
-        return "Detected ${n} high-voltage anomalies in signal stream." ;
+        return `Detected ${n} high-voltage anomalies in signal stream.` ;
     }
     else{
-        return  "Stream stable: no dominant flux events recorded."
+        return  "Stream stable: no dominant flux events recorded." ;
     }
 }
-
 
 console.log(analyzeFluxEvents(transmission));
--- Sequential_Granularity.js
+++ Sequential_Granularity.js
@@ -1,15 +1,15 @@
 const temporalCode = 20240628;
 
-const sequence = temporalCode.toString().split('');
-let LayeredDepth = sequence.length;
+const sequence = temporalCode.toString().split(''); // Convert to string and split into array
+let LayeredDepth = sequence.length; // Initial length of the sequence to check further conditions, 
 
-console.log(sequence);
-console.log(sequence.length);
-console.log(sequence.join(''));
+console.log(sequence); // check
+console.log(LayeredDepth); // check2
+console.log(sequence.join('')); // check3: join back to string checking if it maches original or not, don't wanna take any chances
 
-if (LayeredDepth>=11){
+if (LayeredDepth>=11){ // 2 digit highest binary is 11, so if length is 11 or more, it's Omega
     console.log("Security Level: Omega");
 }
 else {
-    console.log("Security Level: Sigma");
+    console.log("Security Level: Sigma"); // else it's Sigma
 }
